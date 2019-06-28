ab9baa997a8e1137900dcc2af06949d8e274fed3
swiftlet-freedom <yutthadanai.netsodkij@gmail.com>
2019-06-28 17:32:34 +0700

    add i18next translation file

---
+++ src/hello/HelloBody.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+function HelloBody() {
+  const { t } = useTranslation();
+  return <h1>{t('hello')}</h1>
+}
+
+export default HelloBody;
---
+++ src/hello/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import HelloBody from './HelloBody'
+import i18n from './i18n'
+
+function Hello() {
+  return (
+    <HelloBody />
+  )
+}
+
+export default Hello;
